ed94dce700da5a5190afaac6171d946052c07b31
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-11-07 02:10:48 +0300

    Admin Kullanici Yonetim Optimizasyonu

---
+++ OtoServisSatis.Data/Migrations/20231106223225_DataAnnotationEklendi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtoServisSatis.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataAnnotationEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Kullanicilar",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KullaniciAdi",
+                table: "Kullanicilar",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 11, 7, 1, 32, 25, 83, DateTimeKind.Local).AddTicks(8480));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Kullanicilar",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KullaniciAdi",
+                table: "Kullanicilar",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 11, 6, 16, 7, 31, 584, DateTimeKind.Local).AddTicks(8676));
+        }
+    }
+}
--- OtoServisSatis.Entities/Marka.cs
+++ OtoServisSatis.Entities/Marka.cs
@@ -1,8 +1,13 @@
-﻿namespace OtoServisSatis.Entities
-{//3
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OtoServisSatis.Entities
+{
     public class Marka : IEntity
     {
         public int Id { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Adı"), Required(ErrorMessage = "{0} Boş Bırakılamaz!")]
         public string Adi { get; set; }
     }
 }
