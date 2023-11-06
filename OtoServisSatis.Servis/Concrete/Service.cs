7a9c7f0ab7945070ddb02cca1e7a7838061da2c4
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-11-06 19:48:49 +0300

    Servis katmani yapilandirildi

---
+++ OtoServisSatis.Servis/Concrete/Service.cs
@@ -0,0 +1,13 @@
+﻿using OtoServisSatis.Data;
+using OtoServisSatis.Data.Concrete;
+using OtoServisSatis.Entities;
+
+namespace OtoServisSatis.Servis.Concrete
+{
+    public class Service<T> : Repository<T> where T : class, IEntity, new()
+    {
+        public Service(DatabaseContext context) : base(context)
+        {
+        }
+    }
+}
