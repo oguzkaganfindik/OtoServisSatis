6612c4065ca16e27428f1cfbc4e6f88c26a1cdce
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-11-06 22:02:02 +0300

    Admin rol yonetimi yapildi

--- OtoServisSatis.Servis/Concrete/Service.cs
+++ OtoServisSatis.Servis/Concrete/Service.cs
@@ -1,10 +1,11 @@
 ﻿using OtoServisSatis.Data;
 using OtoServisSatis.Data.Concrete;
 using OtoServisSatis.Entities;
+using OtoServisSatis.Servis.Abstract;
 
 namespace OtoServisSatis.Servis.Concrete
 {
-    public class Service<T> : Repository<T> where T : class, IEntity, new()
+    public class Service<T> : Repository<T>, IService<T> where T : class, IEntity, new()
     {
         public Service(DatabaseContext context) : base(context)
         {
